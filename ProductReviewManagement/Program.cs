360eebb077727241a3c969590e69d65cbff1e891
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-17 10:34:20 +0530

    [Ganapati]Product Review Management

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Product review management");
+            List<ProductReview> productreviewlist = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID = 1, UserID = 1,Rating=5,Review="good",islike=true},
+                new ProductReview(){ProductID = 2, UserID = 1,Rating=2,Review="bad",islike=true},
+                new ProductReview(){ProductID = 3, UserID = 1,Rating=5,Review="good",islike=true},
+                new ProductReview(){ProductID = 4, UserID = 1,Rating=4,Review="bad",islike=true},
+                new ProductReview(){ProductID = 5, UserID = 1,Rating=3,Review="good",islike=true}
+            };
+
+            foreach (var products in productreviewlist)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine(products.ProductID + " | " + products.UserID + " | " + products.Rating + " | " + products.Review + " | " + products.islike);
+
+            }
+
+        }
+       
+        
+
+    }
+        
+}
